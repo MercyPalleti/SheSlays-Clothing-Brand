8c8bee95f0e2ce0a5d78200fa491cb0fa845a4c5
MercyPalleti <palletimercy1@gmail.com>
2026-03-15 23:18:08 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://she-slays-frontend.vercel.app/"],
+    origin: ["http://localhost:5173","https://she-slays-frontend.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
